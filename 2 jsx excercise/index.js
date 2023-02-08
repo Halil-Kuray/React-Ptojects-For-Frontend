f486141fbd728424d73ff5bfb543a2c70f7b22cd
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-08 11:52:53 +0100

    first jsx challenge handled

---
+++ 2 jsx excercise/index.js
@@ -0,0 +1,28 @@
+const Navbar = (
+    <nav>
+        <h1>This is a jsx sample</h1>
+        <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Menu</li>
+        </ul>
+    </nav>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+    Navbar,
+    document.getElementById('root')
+    )
