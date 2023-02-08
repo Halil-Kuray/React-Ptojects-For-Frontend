dd8aa032cc7ca143024ce45d9308bce21aa98d92
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-08 11:27:11 +0100

    first example of components were created

--- 1/index.js
+++ 1/index.js
@@ -1,2 +1,27 @@
+function MenuComponent () {
+    return(
+        <main>
+            <h1> This is my header</h1>
+            <p>This is my paragraph</p>
+            <div>This is my div</div>
+        </main>
+    )
+}
 
-ReactDOM.render(<ul> <li>Item 1</li> <li>Item 2</li> </ul>, document.getElementById('root'))
+function MyNavbar () {
+    return (
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    )
+}
+
+
+ReactDOM.render(
+    <main> 
+        <MyNavbar/>
+        <MenuComponent/>
+    </main>,
+    document.getElementById('root'))
