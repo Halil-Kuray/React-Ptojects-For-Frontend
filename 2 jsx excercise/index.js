b8e8315b2186a1cce3cad224711d2d91f86736b4
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-08 15:28:52 +0100

    some jsx variables created and combined as components

--- 2 jsx excercise/index.js
+++ 2 jsx excercise/index.js
@@ -1,5 +1,6 @@
 const Navbar = (
     <nav>
+        <img src="./React-icon.svg.png" width="50px"/>
         <h1>This is a jsx sample</h1>
         <ul>
             <li>About</li>
@@ -8,6 +9,28 @@ const Navbar = (
         </ul>
     </nav>
 )
+
+const Body = (
+    <div>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100 stars on github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>  
+    </div>
+
+)
+
+const App =(
+     <main>
+        {Navbar}
+        {Body}
+    </main>
+    )
+
 
 
 
@@ -18,4 +41,4 @@ const Navbar = (
 //     )
 
 // React version 18 
-ReactDOM.createRoot(document.getElementById('root')).render(Navbar)
+ReactDOM.createRoot(document.getElementById('root')).render(App)
