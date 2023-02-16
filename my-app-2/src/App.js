70927961ac2d9e81cfce5de1c2f7e9cd0cbbd637
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-16 14:19:46 +0100

    project meme s header created

---
+++ my-app-2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProjectMeme from './components/ProjectMeme';
+
+function App() {
+  return (
+    <main className="App">
+      <ProjectMeme/>
+    </main>
+  );
+}
+
+export default App;
---
+++ my-app-2/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='nav'>
+      <div>
+        <h1>Meme Generator</h1>
+      </div>
+      <ul>
+        <li>React-Course</li>
+        <li>Project-3</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
