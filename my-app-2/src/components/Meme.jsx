3f4c40b017a37bab8f983e8f5f252e4997bd92f1
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-16 14:41:41 +0100

    project meme icinde bir form olustudum

---
+++ my-app-2/src/components/Meme.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Meme () {
+    return (
+        <section>
+            <form>
+                <label>first input</label>
+                <input type="text" />
+                <label>Second Input</label>
+                <input type="text" />
+                <button> Get a new meme image</button>
+            </form>
+
+        </section>
+    )
+}
